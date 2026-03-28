cabe0b76c779a64fa55e31463d51f4a21744a4e2
Zimina Ivanna <ivanna.zimina@plus4u.net>
2026-03-28 12:23:43 +0200

    Restructure src and rename challenge folders

---
+++ src/challenges_5_exceptions/Calculator.java
@@ -0,0 +1,10 @@
+/* --- TASK ---
+Write a method, which would ask a user to input numbers and when the user enters something that is not an int then it needs to print a message in the format "SUM = XX AVG = YY“, where XX represents the sum of all entered numbers of type int and YY represents the calculated average of all numbers of type double.
+The method should not have any parameters and should not return anything (void), and it needs to keep reading int numbers from the keyboard until any letter is entered.
+ */
+
+package challenge.task_5_exceptions;
+
+public class Calculator {
+
+}
---
+++ src/challenges_5_exceptions/TextBasedAdventureGame.java
@@ -0,0 +1,12 @@
+/* --- TASK ---
+Write first two levels of menu screens of a text-based adventure game like the Oregon Trail. The first menu level must have at least 5 different choices and the second menu level may just have some arbitrary texts. A user must enter a number to proceed to the second menu level or exit the game, and Q (or any other key of your choice) to return to the first menu level. The game must ignore any entered symbols, except numbers and Q key. The game must have a title, which is shown above the menus.
+An example of a classic text-based adventure game:
+https://www.visitoregon.com/the-oregon-trail-game-online/
+https://en.wikipedia.org/wiki/The_Oregon_Trail_(series)
+*/
+
+package challenge.task_5_exceptions;
+
+public class TextBasedAdventureGame {
+
+}
