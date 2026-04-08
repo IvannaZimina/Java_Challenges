d36148861bed9bc9bb23e79318901154e1d606d7
Zimina Ivanna <ivanna.zimina@plus4u.net>
2026-04-08 14:54:00 +0300

    Update CarpetCostCalculator.java

---
+++ src/challenges_7_Static/CarpetCostCalculator.java
@@ -0,0 +1,16 @@
+/* Carpet Cost Calculator
+The Carpet Company has asked you to write an application that calculates the price of carpeting for rectangular rooms. To calculate the price, you multiply the area of the floor (width times length) by the price per square meter of carpet.
+For example, the area of the floor that is 12 meters long and 10 meters wide is 120 square meters. To cover the floor with a carpet that costs 8€ per square meter would cost 960€.
+Write a class with the name Carpet. The class needs two instance fields with names width and length of type double, and one static field with name squareMeterCost, which would contain the cost of one square meter of  the carpet.
+The class needs to have one constructor with parameters width and length of type double, and it needs to initialize the fields. In case the width or length parameters are less than 0 it needs to set the value to 0.
+Write the instance method named getArea without any parameters, which would return the calculated area (width * length).
+Write the instance method named getTotalCost without any parameters, which would return the calculated total cost to cover the floor with a carpet.
+Create several instances of the Carpet class in your main method with different sizes and print their costs.
+
+*/
+
+package challenges_7_Static;
+
+public class CarpetCostCalculator {
+
+}
